8d646b50142bb081ae886b3d0fe4de79444ea70b
Vaishnavi Bihare <vbihare@gmail.com>
2020-11-28 03:04:17 -0800

    Update

--- Assign_Docker/docker_vb_bash.sh
+++ Assign_Docker/docker_vb_bash.sh
@@ -1,4 +1,7 @@
-DATA=`mysqlshow -h assign_baseball_data -u root baseball | grep -v Wildcard | grep -o baseball`
+# Bash script to check if we have the Baseball data or no, if not create one.
+# Will be used to call our SQL file that calculates the rolling average.
+
+DATA=`mysqlshow -h assign_baseball_data -u root baseball`
 
 # Checking if the database exists or not
 if ["$DATA" == "baseball"] ; then
@@ -6,9 +9,9 @@ if ["$DATA" == "baseball"] ; then
 
 # If the database doesn't exists then we will create the Baseball database
 else
-  mysql -h assign_baseball_data -u root -e "CREATE DATABASE IF NOT EXISTS baseball"
+  mysql -h assign_baseball_data -u root -e \
+  "CREATE DATABASE IF NOT EXISTS baseball"
   mysql -h assign_baseball_data -u root baseball < /Assign_Docker/baseball.sql
-
 fi
 
 # Calling the SQL file to calculate the rolling average
